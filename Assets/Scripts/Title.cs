e5dde91b9d63852ae12f3f457d8b5abc2578c6ef
DragonNexus <zach.claudio@aol.com>
2021-11-29 15:23:53 -0500

    First Uploaded

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scenes/Scene 1");
+        Debug.Log("Game Started");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        Debug.Log("Game Exit");
+    }
+
+    
+}
